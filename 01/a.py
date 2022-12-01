24ea48fe9ac9b55312aedd9c73c17a2d19426a42
parnavh <parnav100sach@gmail.com>
2022-12-01 19:59:12 +0530

    day 1

---
+++ 01/a.py
@@ -0,0 +1,14 @@
+
+def solve(input: str):
+    processed = sorted([sum([int(y) for y in x.split('\n')]) for x in input.split('\n\n')], reverse=True)
+
+    print(processed[0])
+
+def main():
+    with open('./input.txt') as f:
+        input = f.read()
+
+    solve(input)
+
+if __name__ == "__main__":
+    main()
